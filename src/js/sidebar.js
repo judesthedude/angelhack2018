6811b3a8b21166b22bc59405e84dbff1826f36f3
judy <judy@users-MacBook-Pro.local>
2018-06-24 10:47:44 -0400

    sidebar working

--- src/js/sidebar.js
+++
@@ -1,7 +0,0 @@
-$("#menu-toggle").click(function(e) {
-    console.log('3333');
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-    console.log('ahah');
-});
-console.log('asldjfsljf');
