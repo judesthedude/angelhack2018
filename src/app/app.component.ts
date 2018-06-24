4ef6831ee497cc1f0286ebe277083a111e5e7d2b
grassycode <ranjanfbc@gmail.com>
2018-06-24 09:55:28 -0400

    with json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   latitude: number = 43.6532;
   longitude: number = -79.3832;
 
+  constructor(
+    private api: ApiService
+  ){
+
+  }
+
+  ngOnInit() {
+    console.log("test");
+    this.api.getCharity();
+  }
   onClickCoord(e) {
     console.log(e);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { ApiComponent } from './api/api.component';
+import { ApiService } from './api/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAYG9d8fas1qkl6LQv3MvYiYVtUCLXah2Q'
     })
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
